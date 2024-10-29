0b9eb984e05ce7199484163fcd802700dabd22cd
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2024-10-29 16:50:03 -0400

    ....

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,13 +1,61 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { TextInput, Button } from "react-native-paper";
+import { useSignUp } from "@clerk/clerk-expo";
+import { useRouter, Link } from 'expo-router';
 
 
 export default function SignUp() {
+    const { signUp, setActive, isLoaded } = useSignUp();
+    const router = useRouter();
+
     const [pendingVerification, setPendingVerification] = React.useState();
     const [emailAddress, setEmailAddress] = React.useState();
     const [password, setPassword] = React.useState();
     const [code, setCode] = React.useState();
+
+    const onSignUp = async () => {
+        if (!isLoaded) {
+            return;
+        }
+        try {
+            await signUp.create({
+                emailAddress,
+                password,
+            })
+
+            await signUp.prepareEmailAddressVerification({
+                strategy: "email_code",
+            });
+            setPendingVerification(true);
+        } catch (err) {
+            console.error("Signup err", err + ' ', JSON.stringify(err, null, 2));
+        }
+    };
+
+    const onVerifyEmail = async () => {
+        if (!isLoaded) {
+            return;
+        }
+
+        try {
+            const completeSignUp = await signUp.attemptEmailAddressVerification({
+                code,
+            });
+
+            if (completeSignUp.status === "complete") {
+                await setActive({
+                    session: completeSignUp.createdSessionId,
+                });
+                router.push("/(tabs)");
+            } else {
+                console.error(JSON.stringify(err, null, 2));
+            }
+
+        } catch (err) {
+            console.error("Signup err", err.message + " ", JSON.stringify(err, null, 2));
+        }
+    }
     return (
         <View style={styles.container}>
             {!pendingVerification && (
@@ -23,10 +71,11 @@ export default function SignUp() {
                         autoCapitalize='none'
                         value={password}
                         placeholder='Password'
+                        secureTextEntry={true}
                         onChangeText={setPassword}
                     />
                     <Button mode="outlined"
-                        onPress={() => console.log("pressed")}>
+                        onPress={onSignUp}>
                         <Text>Sign Up</Text>
                     </Button>
                 </>
@@ -41,7 +90,7 @@ export default function SignUp() {
                         onChangeText={setCode}
                     />
                     <Button mode="outlined"
-                        onPress={() => console.log("pressed")}>
+                        onPress={onVerifyEmail}>
                         <Text>Verify Email</Text>
                     </Button>
                 </>
