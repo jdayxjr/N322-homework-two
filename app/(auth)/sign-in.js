780be4b1a2ef942105f3a1f0f56cf1fe52478872
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2024-10-30 09:26:19 -0400

    ....

--- app/(auth)/sign-in.js
+++ app/(auth)/sign-in.js
@@ -1,10 +1,68 @@
 import { StyleSheet, Text, View } from 'react-native'
+import { TextInput, Button } from "react-native-paper";
 import React from 'react'
+import { useSignIn } from '@clerk/clerk-expo';
+import { Link, useRouter } from 'expo-router';
 
 export default function SignIn() {
+    const { SignIn, setActive, isLoaded } = useSignIn();
+    const router = useRouter();
+    const [emailAddress, setEmailAddress] = React.useState();
+    const [password, setPassword] = React.useState();
+
+    const onSignIn = React.useCallback(async () => {
+        if (!isLoaded) {
+            return;
+        }
+
+        try {
+            const signInAttempt = await signIn.create({
+                identifier: emailAddress,
+                password: password,
+
+            });
+
+            if (signInAttempt === "complete") {
+                await setActive({
+                    session: signInAttempt.createdSessionId,
+                });
+                router.push("/(tabs)");
+            } else {
+                console.error(JSON.stringify(signInAttempt, null, 2));
+            }
+
+        } catch (err) {
+            console.error("Sign in err", err + ' ', JSON.stringify(err, null, 2));
+        }
+    }, [isLoaded, emailAddress, password]);
     return (
         <View style={styles.container}>
-            <Text>sign-in</Text>
+            <View>
+                <TextInput
+                    autoCapitalize='none'
+                    value={emailAddress}
+                    keyboardType='emal-address'
+                    placeholder='Email address'
+                    onChangeText={setEmailAddress}
+                />
+                <TextInput
+                    autoCapitalize='none'
+                    value={password}
+                    placeholder='Password'
+                    secureTextEntry={true}
+                    onChangeText={setPassword}
+                />
+                <Button mode="outlined"
+                    onPress={onSignIn}>
+                    <Text>Sign Up</Text>
+                </Button>
+            </View>
+            <View>
+                <Text>Don't have an account?</Text>
+                <Link href="/sign-up">
+                    <Text>Sign up</Text>
+                </Link>
+            </View>
         </View>
     )
 }
